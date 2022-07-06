5e376a05c3a94bbcca9e8d67d884ca98ae6a2c1b
Clair-Hawkes <hawkesj7@gmail.com>
2022-07-06 11:44:20 -0700

    Add Insomnia Routes and switched Mac vs Windows DB connection

--- config.js
+++ config.js
@@ -10,17 +10,19 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
-// function getDatabaseUri() {
-//   return (process.env.NODE_ENV === "test")
-//       ? "jobly_test"
-//       : process.env.DATABASE_URL || "jobly";
-// }
-
+/**Mac Databse */
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
-    ? "postgresql://jasano:foofoo@localhost/jobly_test"
-    : "postgresql://jasano:foofoo@localhost/jobly";
+      ? "jobly_test"
+      : process.env.DATABASE_URL || "jobly";
 }
+
+/** Windows database */
+// function getDatabaseUri() {
+//   return (process.env.NODE_ENV === "test")
+//     ? "postgresql://jasano:foofoo@localhost/jobly_test"
+//     : "postgresql://jasano:foofoo@localhost/jobly";
+// }
 
 
 
